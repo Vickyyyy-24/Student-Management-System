750d5f55b44a295d033e64ad0647275ff52b873a
Vickyyyy-24 <vikasj.pvt23@gmail.com>
2026-06-26 09:21:42 +0530

    backend ready

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ require("dotenv").config()
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const cors = require("cors");
 
 
+app.use(cors());
+// app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads"));
 
 // start API route
 app.get("/", (req, res) => {
